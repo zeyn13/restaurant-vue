151e4c55691112bc42ca5b3ff028632b030913dd
Zeynep Lika <95577898+zeyn13@users.noreply.github.com>
2023-03-02 12:58:06 +0100

    photos

--- src/routers.js
+++ src/routers.js
@@ -1,4 +1,4 @@
-import Home from "./components/Home.vue";
+import Home from "./components/Home.html";
 import SignUp from "./components/SignUp.vue";
 import { createRouter, createWebHistory } from "vue-router";
 
