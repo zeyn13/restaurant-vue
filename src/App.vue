e295341e22f1e74903ed7cddb3e46aba76a6116a
Zeynep Lika <95577898+zeyn13@users.noreply.github.com>
2023-03-02 13:33:49 +0100

    homepage start

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,16 @@
 </template>
 
 <script>
+import NavBar from "./components/NavBar.vue";
+
 export default {
   name: "App",
+  components: {
+    NavBar,
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/css/global_style.css";
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,7 @@
 <template>
-    
+  <div class="header">
+    <router-link @click="scrollToTop()" to="/" class="logo"
+      ><img src="../assets/images/taco-logo.png" alt="" />Food
+    </router-link>
+  </div>
 </template>
