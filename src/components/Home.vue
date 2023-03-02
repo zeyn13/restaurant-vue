dac10b4c65afbc30b94f57489d012f15d3d11c96
BlertaKryeziu <bk52307@ubt-uni.net>
2023-03-02 15:35:12 +0100

    style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -199,4 +199,143 @@ export default {
   right: -15rem;
   width: 80vw;
 }
+
+
+/*home category*/
+.home-category{
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(10rem, 1fr));
+  gap: 1.5rem;
+  padding-bottom: 5rem;
+}
+
+.home-category .box{
+  padding: 2rem;
+  text-align: center;
+  border-radius: .5rem;
+  background: #f7f7f7;
+}
+
+.home-category .box:hover{
+  background: #27ae60;
+}
+
+.home-category .box:hover h3{
+  color: #fff;
+}
+
+.home-category .box img{
+  height: 7rem;
+}
+.home-category .box h3 {
+    font-size: 1.8rem;
+    color: #130f40;
+}
+
+/*home banner */
+.home-banner .row-banner{
+  background: url(../assets/images/row-banner.png) no-repeat;
+  height: 45rem;
+  background-size: cover;
+  background-position: center;
+  position: relative;
+}
+
+.home-banner .row-banner .content{
+  position: absolute;
+  top: 50%;
+  left: 7%;
+  transform: translateY(-50%);
+}
+
+.home-banner .row-banner .content span{
+  font-family: 'Satisfy' cursive;
+  font-size: 4rem;
+  color: #27ae60;
+  color: #130f40;
+}
+
+.home-banner .row-banner .content h3 {
+  font-size: 6rem;
+  color: red;
+  text-transform: uppercase;
+}
+
+.home-banner .row-banner .content p{
+  font-size: 2rem;
+  padding-bottom: 1rem;
+}
+
+.home-banner .grid-banner .grid{
+  border-radius: 1rem;
+  overflow: hidden;
+  height: 45rem;
+}
+
+.home-banner .grid-banner .grid:hover img{
+  transform: scale(1.2);
+}
+
+.home-banner .grid-banner .grid-img {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+
+.home-banner .grid-banner .grid .content{
+  position: absolute;
+  top: 2rem;
+  padding: 0 2 rem;
+}
+
+.home-banner .grid-banner .grid .content.center {
+  text-align: center;
+  width: 100%;
+}
+
+.home-banner .grid-banner .grid .content.center span{
+  color: #666;
+}
+
+.home-banner .grid-banner .grid .content.center h3{
+  color: #130f40;
+}
+
+.home-banner .grid-banner .grid .content span{
+  font-size: 2.5rem;
+  color: #fff;
+}
+
+.home-banner .grid-banner .grid .content h3{
+  font-size: 3rem;
+  color: #fff;
+  padding-top: .5rem;
+}
+
+.home-about {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 2rem;
+  background: #f7f7f7;
+}
+.home-about .image{
+  flex: 1 1 40rem;
+}
+
+.home-about .image img{
+  width: 100%;
+}
+
+.home-about .content{
+  flex: 1 1 40rem;
+}
+
+.home-about .content span{
+  font-family: 'Satisfy' cursive;
+  font-size: 3rem;
+  color: #27ae60;
+}
+
+
 </style>
