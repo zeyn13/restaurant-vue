460270c8b50588569c004075d8918af099d3a3c2
Zeynep Lika <95577898+zeyn13@users.noreply.github.com>
2023-03-03 23:43:06 +0100

    change

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "../pages/Home.vue";
 import Login from "../pages/Login.vue";
+import Register from "../pages/Register.vue";
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login,
+  },
+  {
+    path: "/register",
+    name: "Register",
+    component: Register,
   },
 ];
 
