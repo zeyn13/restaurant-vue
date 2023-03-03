195330a56ae5cf8c2b09951b08b63976141b3bec
Zeynep Lika <95577898+zeyn13@users.noreply.github.com>
2023-03-03 13:45:42 +0100

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,14 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 import Home from "../pages/Home.vue";
-import Register from "../pages/Register.vue";
+
 import Login from "../pages/Login.vue";
 
 const routes = [
   {
-    name: "Home",
-    component: Home,
     path: "/",
-  },
-  {
-    name: "Register",
-    component: Register,
-    path: "/register",
-  },
-  {
-    name: "Login",
-    component: Login,
-    path: "/login",
+    name: "Home",
+    component: "Home",
   },
 ];
 
